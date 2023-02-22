78ea248d8b3ebc05f905c1a7260b231e3c757e05
Sara Morison <rob.morison32@gmail.com>
2023-02-22 14:43:43 -0500

    Created local settings file + Connected database + Made initial migration

--- songs_project/settings.py
+++ songs_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--h#ubj*8=+sod+s^7@-4@_*b^fjifycfnq$q@8b541xyc=7g6l'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,15 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'songs_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -125,3 +116,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+try:
+    from songs_project.local_settings import *
+except ImportError:
+    pass
